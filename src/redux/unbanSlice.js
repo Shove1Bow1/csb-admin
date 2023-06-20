414632144372b8c5289188999d7e284a27ab8aa5
Shove1Bow1 <fireblaster2001@gmail.com>
2023-06-20 14:40:25 +0700

    stupid

--- src/redux/unbanSlice.js
+++ src/redux/unbanSlice.js
@@ -11,7 +11,6 @@ export const unbanSlice = createSlice({
       // doesn't actually mutate the state because it uses the Immer library,
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
-      console.log(actions.payload);
       state.value = actions.payload.reverse();
     },
   },
