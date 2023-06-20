e51f9b43c12844efd22b36966a84e3f75e1c86ff
Shove1Bow1 <fireblaster2001@gmail.com>
2023-06-20 15:08:55 +0700

    push

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -10,10 +10,21 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
   databaseURL:process.env.REACT_APP_DATABASE_URL_SECOND
 };
+const secondConfig={
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  databaseURL:process.env.REACT_APP_DATABASE_URL
+}
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig,'first');
+const second=initializeApp(secondConfig,'second');
 // Initialize Realtime Database and get a reference to the service
 const dbthongke = getDatabase(app);
 
-const database = getDatabase(app, process.env.REACT_APP_DATABASE_URL);
+const database = getDatabase(second);
 export { database, dbthongke };
